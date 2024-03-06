6513a387ee77e07be112e357dcbe69a284260d64
unknown <jithinbp007@gmail.com>
2024-03-06 12:05:10 +0530

    setting up the base file

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: "hsl(var(--color-primary-blue))",
+        primaryCyan: "hsl(var(--color-primary-cyan))",
+        primaryPurple: "hsl(var(--color-primary-purple))",
+
+        light: {
+          100: "hsl(var(--color-light-100))",
+          200: "hsl(var(--color-light-200))",
+          300: "hsl(var(--color-light-300))",
+          400: "hsl(var(--color-light-400))",
+          500: "hsl(var(--color-light-500))",
+        },
+        dark: {
+          100: "hsl(var(--color-dark-100))",
+          200: "hsl(var(--color-dark-200))",
+          300: "hsl(var(--color-dark-300))",
+          400: "hsl(var(--color-dark-400))",
+          500: "hsl(var(--color-dark-500))",
+          600: "hsl(var(--color-dark-600))",
+          700: "hsl(var(--color-dark-700))",
+        },
+      },
+    },
+  },
+
+  darkMode: "selector",
+  plugins: [],
+};
